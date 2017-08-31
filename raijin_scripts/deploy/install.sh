a613e59838ba249adf965b5ca3869f686920aeea
Harshu Rampur <hr8696@raijin6.(none)>
2017-08-31 18:00:49 +1000

    ansible replaced ny a python script

--- raijin_scripts/deploy/install.sh
+++ raijin_scripts/deploy/install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mkdir -p deploy
-
 module load python3/3.5.2
-python3 -m venv trigger_env
-source trigger_env/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 pip install --upgrade pip
-pip install ansible
+pip install jinja2
 pip install boto3
 deactivate
