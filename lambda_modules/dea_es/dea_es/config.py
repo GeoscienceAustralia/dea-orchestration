2ac3030f3fca4cd489cd5de524fdddd5a14e7b81
Alex Vincent <alexander.vincent@ga.gov.au>
2017-09-19 08:22:12 +1000

    adding environment check to es credentials

--- lambda_modules/dea_es/dea_es/config.py
+++ lambda_modules/dea_es/dea_es/config.py
@@ -1,6 +1,6 @@
 import os
 
-from dea_raijin.config import LOGGING_PREFIX, AWS_REGION
+from dea_raijin.config import LOGGING_PREFIX, AWS_REGION, DEA_ENVIRONMENT
 from dea_raijin.auth import get_ssm_parameter
 
 _DEFAULT_DEA_ES = 'search-digitalearthaustralia-lz7w5p3eakto7wrzkmg677yebm.ap-southeast-2.es.amazonaws.com'
@@ -12,7 +12,7 @@ _AWS_ES_ACCESS_KEY = os.environ.get('DEA_AWS_ES_ACCESS_KEY')
 if _AWS_ES_ACCESS_KEY:
     # Run if the environment parameter is set
     _AWS_PRIVATE_KEY = get_ssm_parameter(os.environ.get('DEA_AWS_ES_PRIVATE_KEY'))
-else:
+else if not _AWS_ES_ACCESS_KEY and DEA_ENVIRONMENT == 'dev':
     # Allow users to test with their local aws credentials
     import logging
     logger = logging.getLogger(__name__)
@@ -28,3 +28,5 @@ else:
                     _AWS_PRIVATE_KEY = v.strip()
             except ValueError:
                 pass  # environment config
+else:
+    pass  # test environment
