21d82d37dd43e8ec1045141841fdd5a576bc52cb
Alex Vincent <avin7233@uni.sydney.edu.au>
2017-10-10 11:46:54 +1100

    Turn on shell linter and fixed existing issues (#15)

--- scripts/check-code.sh
+++ scripts/check-code.sh
@@ -4,16 +4,22 @@
 set -eu
 set -x
 {
-    LINT_ARGS="lambda_modules/* $(find raijin_scripts/. lambda_functions/. -iname '*.py')"
+    LINT_ARGS=(lambda_modules/* $(find raijin_scripts/. lambda_functions/. -iname '*.py'))
 } &> /dev/null
 
-python3 -m pep8 $LINT_ARGS 
+# Python linting
+python3 -m pep8 "${LINT_ARGS[@]}"
 
-python3 -m pylint -j 2 --reports no $LINT_ARGS 
+python3 -m pylint -j 2 --reports no "${LINT_ARGS[@]}"
+
+# Shell linting
+shellcheck -e SC1071,SC1090,SC1091 scripts/*
+shellcheck -e SC1071,SC1090,SC1091 raijin_scripts/*/run
+shellcheck -e SC1071,SC1090,SC1091 raijin_scripts/*/*.sh
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-python3 -m pytest -r sx --doctest-ignore-import-errors --durations=5 lambda_functions lambda_modules $@
+python3 -m pytest -r sx --doctest-ignore-import-errors --durations=5 lambda_functions lambda_modules "$@"
 
 set +x
 
@@ -21,6 +27,6 @@ set +x
 if which yamllint;
 then
     set -x
-    yamllint $(find . \( -iname '*.yaml' -o -iname '*.yml' \) )
+    yamllint "$(find . \( -iname '*.yaml' -o -iname '*.yml' \) )"
     set +x
 fi
