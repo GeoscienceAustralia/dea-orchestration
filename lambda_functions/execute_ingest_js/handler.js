e7b9a3a93644c1ff57e68c7c463b6e64ee1313c7
Damien Ayers <damien@omad.net>
2018-06-18 10:47:28 +1000

    Make the serverless js run different ssh commands depending on event.
    Hopefully this will reduce code duplication, and allow us to centralise
    most of our orchestration configuration into one `serverless.yml` file.
    Which should be much easier to audit.

--- lambda_functions/execute_ingest_js/handler.js
+++ lambda_functions/execute_ingest_js/handler.js
@@ -9,9 +9,11 @@ const ssm = new AWS.SSM();
 const hostkey = process.env.hostkey;
 const userkey = process.env.userkey;
 const pkey = process.env.pkey;
-const dea_module = process.env.DEA_MODULE;
-const project = process.env.PROJECT;
-const queue = process.env.QUEUE;
+// We could possibly go back to environment variables here instead of
+// passing these values in via the event. Still to be decided.
+// const dea_module = process.env.DEA_MODULE;
+// const project = process.env.PROJECT;
+// const queue = process.env.QUEUE;
 
 let req = {
     Names: [hostkey, userkey, pkey],
@@ -20,26 +22,28 @@ let req = {
 let keys = ssm.getParameters(req).promise();
 
 function create_execution_string(event) {
+    /* Turn an event into an ssh execution string.
+
+      Excepts event['command'] to be the command name,
+      and all other elements to be named cli arguments. eg:
+
+      let event = { command: 'mycommand', arg1: 'myarg' };
+
+      Will result in a command of:
+      'mycommand --arg1 myarg'
+     */
+    let command_name = event['command'];
+    delete event['command'];
     let args = _(event.args)
         .transform((r, v, k) => r['--' + k.replace('_', '-')] = `'${v}'`)
         .toPairs().flatten().join(' ');
-    return event.command + ' ' + args;
+    return command_name + ' ' + args;
 }
 // See https://hackernoon.com/you-should-use-ssm-parameter-store-over-lambda-env-variables-5197fc6ea45b
 // For a more rigorous way of loading data from SSM using KSM
 // Or look at https://github.com/n1ru4l/ssm-parameter-env
 
-module.exports.execute_ingest = (event, context, callback) => {
-    let product = event['product'];
-    let year = event['year'];
-
-    if (typeof year !== 'string') {
-        return callback('Year not specified');
-    }
-    if (typeof product !== 'string') {
-        return callback('Product not specified');
-    }
-
+module.exports.execute_ssh_command = (event, context, callback) => {
     keys.then((data) => {
         let params = {};
         for (let p of data.Parameters) {
@@ -52,12 +56,13 @@ module.exports.execute_ingest = (event, context, callback) => {
             key: params[pkey],
         });
 
-        let command = `execute_ingest --dea-module ${dea_module} --project ${project} --queue ${queue} --product ${product} --year ${year}`;
+        let command = create_execution_string(event);
+//        let command = `execute_ingest --dea-module ${dea_module} --project ${project} --queue ${queue} --product ${product} --year ${year}`;
         console.log(`Executing: ${command}`);
         ssh.exec(command, {
             out: function(stdout) {
                 console.log(stdout);
-                const response = { statusCode: 200, body: 'Ingestion queued.' };
+                const response = { statusCode: 200, body: 'SSH command executed.' };
                 callback(null, response);
             },
             err: function(stderr) {
