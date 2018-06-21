50d65060927b0470ec76c8949c9fb2d7da37dbb5
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-06-21 12:10:49 +1000

    Append sync to ingest script and fix aws execution error

--- lambda_functions/execute_sync_ingest_js/handler.js
+++ lambda_functions/execute_sync_ingest_js/handler.js
@@ -30,6 +30,45 @@ function create_execution_string(event) {
 // See https://hackernoon.com/you-should-use-ssm-parameter-store-over-lambda-env-variables-5197fc6ea45b
 // For a more advanced way of loading data from SSM using KSM
 // Or look at https://github.com/n1ru4l/ssm-parameter-env
+module.exports.execute_sync = (event, context, callback) => {
+        let req = {
+                   Names: [hostkey, userkey, pkey, pphrase],
+                   WithDecryption: true
+        };
+        let keys = ssm.getParameters(req).promise();
+
+        keys.catch(function(err) {
+            console.log(err);
+        });
+        keys.then((data) => {
+            let params = {};
+            for (let p of data.Parameters) {
+                 params[p.Name] = p.Value;
+            }
+            console.log(`Host key: ${params[hostkey]}`);
+            console.log(`User key: ${params[userkey]}`);
+            let ssh = new SSH({
+                               host: params[hostkey],
+                               user: params[userkey],
+                               key: params[pkey],
+                               pass: params[pphrase],
+                               });
+
+            let command = create_execution_string(event);
+            console.log(`Executing: ${command}`);
+            ssh.exec(command, {
+                               out: function(stdout) {
+                               console.log(stdout);
+                               const response = { statusCode: 200, body: 'SSH command executed.' };
+                               callback(null, response);
+                                   },
+                               err: function(stderr) {
+                                         callback(`SSH Failed ${stderr}`);
+                                     }
+                               }).start();
+                             });
+        };
+
 module.exports.execute_ingest = (event, context, callback) => {
         let req = {
                    Names: [hostkey, userkey, pkey, pphrase],
