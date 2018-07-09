47e3bc9f1e8773661b8a569b0a042cbd0b5b5d14
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-07-09 18:12:36 +1000

    Add dea_sync test script to dea environment raijin scripts. Modify scripts to log correctly within the directory

--- raijin_scripts/test_deaenv_index_ingest/dea_testscripts/cp_indingest_yaml_files.sh
+++ raijin_scripts/test_deaenv_index_ingest/dea_testscripts/cp_indingest_yaml_files.sh
@@ -3,7 +3,6 @@
 WORKDIR="$1"
 
 # Remove previous stored config files
-[ -d "$WORKDIR/output_files/idx_ingest" ] && rm -r "$WORKDIR/output_files/idx_ingest"
 [ -d "$WORKDIR/ingest_configfiles" ] && rm -r "$WORKDIR/ingest_configfiles"
 
 # Create new empty directory
@@ -15,10 +14,7 @@ declare -a dc_yaml_array=("ls8_nbart_albers.yaml"
                           "ls8_pq_albers.yaml"
                           "ls7_nbart_albers.yaml"
                           "ls7_nbar_albers.yaml"
-                          "ls7_pq_albers.yaml"
-                          "ls5_nbar_albers.yaml"
-                          "ls5_nbart_albers.yaml"
-                          "ls5_pq_albers.yaml")
+                          "ls7_pq_albers.yaml")
 
 # Replace NBAR/NBART/PQ product output location in the yaml file
 cd "$WORKDIR"/ingest_configfiles || exit 0
@@ -28,6 +24,6 @@ do
   yaml_filename=$(basename "$INGEST_CONF_DIR")
 
   wget -q "$INGEST_CONF_DIR"
-  sed -e 's,location: .*,location: "'"$WORKDIR"'/output_files/idx_ingest/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,location: .*,location: "'"$WORKDIR"'/work",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
   sed -e 's,description:,metadata_type: eo'"\\n\\"'description:,'  "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
 done
--- raijin_scripts/test_deaenv_stats/dea_testscripts/cp_statsyaml_files.sh
+++ raijin_scripts/test_deaenv_stats/dea_testscripts/cp_statsyaml_files.sh
@@ -3,7 +3,6 @@
 WORKDIR="$1"
 
 # Remove previous stored config files
-[ -d "$WORKDIR/output_files/stats" ] && rm -r "$WORKDIR/output_files/stats"
 [ -d "$WORKDIR/stats_configfiles" ] && rm -r "$WORKDIR/stats_configfiles"
 
 # Create new empty directory
@@ -12,8 +11,10 @@ mkdir -p "$WORKDIR"/stats_configfiles
 ## Declare datacube stats array of yaml files to download
 declare -a fcstats_yaml_array=("fc_stats_annual.yaml"
                                "fc_percentile_albers_seasonal.yaml"
-                               "fc_percentile_albers_annual.yaml"
-                               "fc_percentile_albers.yaml")
+                               "fc_percentile_albers_annual.yaml")
+
+## Declare datacube stats array of yaml files to download
+declare -a fcpercentiltstats_yaml_array=("fc_percentile_albers.yaml")
 
 declare -a nbarstats_yaml_array=("nbar_stats.yaml")
 
@@ -25,7 +26,24 @@ do
   yaml_filename=$(basename "$FC_STATS_CONF_DIR")
 
   wget -q "$FC_STATS_CONF_DIR"
-  sed -e 's,location: .*,location: "'"$WORKDIR"'/output_files/stats/fc/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,location: .*,location: "'"$WORKDIR"'/fc_stats/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,2018-01-01,2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,  start_date: .*,  start_date: 2018-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,  end_date: .*,  end_date: 2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,2017-01-01,2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+done
+
+for i in "${fcpercentiltstats_yaml_array[@]}"
+do
+  FC_STATS_CONF_DIR="https://github.com/GeoscienceAustralia/datacube-stats/raw/master/configurations/fc/$i"
+  yaml_filename=$(basename "$FC_STATS_CONF_DIR")
+
+  wget -q "$FC_STATS_CONF_DIR"
+  sed -e 's,location: .*,location: "'"$WORKDIR"'/fc_stats/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,2018-01-01,2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,  start_date: .*,  start_date: 2018-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,  end_date: .*,  end_date: 2019-01-01\n  stats_duration: 3m\n  step_size: 3m,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,2017-01-01,2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
 done
 
 # Replace NBAR stats product output location in the yaml file
@@ -36,7 +54,7 @@ do
   yaml_filename=$(basename "$NBAR_STATS_CONF_DIR")
 
   wget -q "$NBAR_STATS_CONF_DIR"
-  sed -e 's,location: .*,location: "'"$WORKDIR"'/output_files/stats/nbar/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
+  sed -e 's,location: .*,location: "'"$WORKDIR"'/nbar_stats/001",' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
   sed -e 's,  start_date: .*,  start_date: 2018-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
   sed -e 's,  end_date: .*,  end_date: 2019-01-01,' "$yaml_filename" > "$yaml_filename.tmp" && mv "$yaml_filename.tmp" "$yaml_filename"
 done
