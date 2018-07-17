ac5536b701ce13bb9bd830f984379d5e6289e151
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-07-17 11:35:45 +1000

    Remove blank lines and spaces in the blank line

--- lambda_modules/dea_raijin/dea_raijin/utils.py
+++ lambda_modules/dea_raijin/dea_raijin/utils.py
@@ -7,7 +7,7 @@ def timestr_to_seconds(time_str):
         parts = [int(t) for t in time_str.split(':')]
     except TypeError:
         raise RuntimeError('timestamp ill formatted')
-    
+
     ret_val = None
     if len(parts) == 2:
         ret_val = parts[0] * (60**2) + parts[1] * (60)
