819ebac07953999cf8bc837db233fd6886f63045
Damien Ayers <damien@omad.net>
2018-08-01 15:03:50 +1000

    Fix linting errors

--- raijin_scripts/deploy/reinstall_miniconda.sh
+++ raijin_scripts/deploy/reinstall_miniconda.sh
@@ -3,6 +3,6 @@
 rm -rf /g/data/v10/private/miniconda3
 curl -o "$TMPDIR/miniconda.sh" https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x "$TMPDIR/miniconda.sh"
-cd "$TMPDIR"
+cd "$TMPDIR" || exit
 ./miniconda.sh -b -f -u -p /g/data/v10/private/miniconda3
 /g/data/v10/private/miniconda3/bin/conda update -y -n base conda
