37c6d8f46f5e55b0b8222318e764cb9825895961
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-08-24 10:57:26 +1000

    Comment jupyter lab build, as jupyter lab does not support production enviroment

--- raijin_scripts/deploy/reinstall_miniconda.sh
+++ raijin_scripts/deploy/reinstall_miniconda.sh
@@ -7,12 +7,6 @@ curl -o "$TMPDIR/miniconda.sh" https://repo.continuum.io/miniconda/Miniconda3-la
 chmod +x "$TMPDIR/miniconda.sh"
 cd "$TMPDIR" || exit
 ./miniconda.sh -b -f -u -p "$MINICONDA_PATH"
-#"$MINICONDA_PATH"/bin/conda update -y -n base --all
-"$MINICONDA_PATH"/bin/conda install -y -c conda-forge pip
-"$MINICONDA_PATH"/bin/conda install -y -c anaconda memory_profiler
-"$MINICONDA_PATH"/bin/conda install -y -c conda-forge ipywidgets widgetsnbextension 'nodejs<10' notebook nb_conda_kernels jupyterlab=0.33.0
-
-#"$MINICONDA_PATH"/bin/conda install -c conda-forge --override-channels --yes python=3.6 pip cookiecutter=1.6 notebook=5.5 pandas=0.23 nodejs=9.11 jupyterlab bqplot ipyvolume pythreejs
-#"$MINICONDA_PATH"/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-threejs ipyvolume bqplot @jupyterlab/geojson-extension @jupyterlab/fasta-extension
-#"$MINICONDA_PATH"/bin/conda install -y -c conda-forge yarn=1.6.0
+"$MINICONDA_PATH"/bin/conda update -y -n base --all
+"$MINICONDA_PATH"/bin/conda install -y --override-channels -c conda-forge pip tar
 chmod -R ug+rw "$MINICONDA_PATH"
