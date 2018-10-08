8c5bae939a72f091618204924d27ae93f9760126
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-10-08 16:43:25 +1100

    Use str.startswith method instead of str.partition method

--- raijin_scripts/deploy/dea/datacube-ensure-user.py
+++ raijin_scripts/deploy/dea/datacube-ensure-user.py
@@ -79,6 +79,7 @@ def find_credentials(pgpass, host, dbcreds):
     else:
         with pgpass.open() as src:
             for line in src:
+                # Ignore comments and empty lines
                 if not line.startswith('#') and line.strip():
                     creds = DBCreds(*line.strip().split(':'))
                     if creds.host == host and creds.username == dbcreds.username:
