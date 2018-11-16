358333cf1316582d2c43d8a2eb7d189fbbc6bdc3
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-11-16 13:00:32 +1100

    refactoring

--- lambda_functions/stac/notify_to_stac_queue.py
+++ lambda_functions/stac/notify_to_stac_queue.py
@@ -18,14 +18,13 @@ def s3_key_to_stac_queue(sqs_client, queue_url, bucket, s3_key):
     )
 
 
-def messages_to_sqs(s3_keys, queue_url):
+def messages_to_sqs(s3_keys, bucket, queue_url):
     sqs = boto3.client('sqs')
 
     for item in s3_keys:
-        if Path(item.Key).suffix == '.yaml':
+        if Path(item).suffix == '.yaml':
             # send a message to SQS
-            s3_key_to_stac_queue(sqs, queue_url,
-                                 item.Bucket, item.Key)
+            s3_key_to_stac_queue(sqs, queue_url, bucket, item)
 
 
 @click.group(help=__doc__)
@@ -37,23 +36,27 @@ def cli():
 @click.option('--inventory-manifest', '-i', help="The manifest of AWS inventory list")
 @click.option('--queue-url', '-q', help="AWS sqs url")
 @click.option('--s3-keys', '-s', nargs=-1, type=click.Path(), help="List of s3 keys")
-@click.option('--bucket', '-b', help="AWS bucket")
+@click.option('--bucket', '-b', required=True, help="AWS bucket")
 def message_stac_queue(inventory_manifest, queue_url, s3_keys, bucket):
     """
     Send messages (yaml s3 keys) to stac_queue
     """
     assert xor(bool(inventory_manifest), bool(s3_keys)), "Use one of inventory-manifest or s3-keys"
     assert s3_keys and bucket, "The s3-keys must be specified with the associated bucket"
-    if s3_keys:
-        key_list = [SimpleNamespace(Bucket=bucket, Key=key) for key in s3_keys]
-    else:
+
+    def _shed_bucket(keys):
+        for item in keys:
+            yield item.Key
+
+    if not s3_keys:
         if not inventory_manifest:
             inventory_manifest = 's3://dea-public-data-inventory/dea-public-data/dea-public-data-csv-inventory/'
         s3 = make_s3_client()
-        key_list = list_inventory(inventory_manifest, s3=s3)
+        s3_keys = _shed_bucket(list_inventory(inventory_manifest, s3=s3))
+
     if not queue_url:
         queue_url = "https://sqs.ap-southeast-2.amazonaws.com/451924316694/static-stac-queue"
-    messages_to_sqs(key_list, queue_url)
+    messages_to_sqs(s3_keys, bucket, queue_url)
 
 
 if __name__ == '__main__':
