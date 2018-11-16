cfb0f00a70409e0bfa12492a03c6f007ff5d80b2
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-11-16 14:09:40 +1100

    basic testing done

--- lambda_functions/stac/notify_to_stac_queue.py
+++ lambda_functions/stac/notify_to_stac_queue.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-from operator import xor
-from types import SimpleNamespace
 import json
 import boto3
 from dea.aws import make_s3_client
@@ -27,22 +25,16 @@ def messages_to_sqs(s3_keys, bucket, queue_url):
             s3_key_to_stac_queue(sqs, queue_url, bucket, item)
 
 
-@click.group(help=__doc__)
-def cli():
-    pass
-
-
-@cli.command()
+@click.command()
 @click.option('--inventory-manifest', '-i', help="The manifest of AWS inventory list")
 @click.option('--queue-url', '-q', help="AWS sqs url")
-@click.option('--s3-keys', '-s', nargs=-1, type=click.Path(), help="List of s3 keys")
 @click.option('--bucket', '-b', required=True, help="AWS bucket")
-def message_stac_queue(inventory_manifest, queue_url, s3_keys, bucket):
+@click.argument('s3-keys', nargs=-1, type=click.Path())
+def cli(inventory_manifest, queue_url, bucket, s3_keys):
     """
     Send messages (yaml s3 keys) to stac_queue
     """
-    assert xor(bool(inventory_manifest), bool(s3_keys)), "Use one of inventory-manifest or s3-keys"
-    assert s3_keys and bucket, "The s3-keys must be specified with the associated bucket"
+    assert not (inventory_manifest and s3_keys), "Use one of inventory-manifest or s3-keys"
 
     def _shed_bucket(keys):
         for item in keys:
