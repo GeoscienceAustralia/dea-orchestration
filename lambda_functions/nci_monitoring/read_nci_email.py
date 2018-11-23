dc335a8ebe3dc089e4db029340dbcc52e9c29d31
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-11-23 11:13:27 +1100

    Fix travis failures

--- lambda_functions/nci_monitoring/read_nci_email.py
+++ lambda_functions/nci_monitoring/read_nci_email.py
@@ -6,8 +6,8 @@ import copy
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
 
 ES_INDEX = 'nci-monitor-'
 ES_HOST = 'search-digitalearthaustralia-lz7w5p3eakto7wrzkmg677yebm.ap-southeast-2.es.amazonaws.com'
@@ -15,42 +15,81 @@ AWS_REGION = 'ap-southeast-2'
 ES_PORT = 443
 SIZE = {'KB': 1024, 'MB': 1024**2, 'GB': 1024**3, 'TB': 1024**4}
 
-s3 = boto3.resource('s3')
+S3 = boto3.resource('s3')
 
 
-def push_metadata_to_es(doc):
-    logger.info(f'Connecting to the ES Endpoint, {ES_HOST}:{ES_PORT}')
-    try:
-        credentials = boto3.Session().get_credentials()
-        auth = AWS4Auth(credentials.access_key, credentials.secret_key,
-                        AWS_REGION, 'es', session_token=credentials.token)
+def _push_metadata_to_es(doc):
+    LOG.info('Connecting to the ES Endpoint, {%s}:{%s}', ES_HOST, ES_PORT)
+    credentials = boto3.Session().get_credentials()
+    auth = AWS4Auth(credentials.access_key, credentials.secret_key,
+                    AWS_REGION, 'es', session_token=credentials.token)
 
-        # Connect to the Amazon ES domain endpoint
-        es = Elasticsearch(
-            hosts=[{'host': ES_HOST, 'port': ES_PORT}],
-            http_auth=auth,
-            use_ssl=True,
-            verify_certs=True,
-            connection_class=RequestsHttpConnection)
-    except Exception as exp:
-        logger.info(f"Unable to connect to the ES Endpoint, {ES_HOST}:{ES_PORT}")
-        logger.info(exp)
-        exit(3)
+    # Connect to the Amazon ES domain endpoint
+    es = Elasticsearch(
+        hosts=[{'host': ES_HOST, 'port': ES_PORT}],
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection)
 
     now = datetime.utcnow()
     doc = copy.deepcopy(doc)
     doc.update({
-            '@timestamp': now.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        '@timestamp': now.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
         })
 
-    logger.info(doc)
+    LOG.info(doc)
     index = ES_INDEX + now.strftime('%Y-%m-%d')
 
     # Create an index (if one has not already been created) and
     # write the metadata document into AWS Elasticsearch
     retval = es.index(index=index, doc_type='doc', body=doc)
-    logger.info(retval)
+    LOG.info(retval)
     es.indices.refresh(index=index)
+
+
+def _fetch_job_info(email_body):
+    # Enable CPU calculation flag, if we have walltime, cputime, and ncpus in the email body
+    cflag = False
+
+    cpu_time = walltime = ncpus = exit_status = 0, 0, 0, 0
+    job_id = job_name = exe_status = "NA", "NA", "NA"
+    mem_used = vmem_used = "0kb", "0kb"
+    job_efficiency = 0.0
+
+    # Report when the job started
+    for line in email_body.split('\r\n'):
+        if "resources_used.walltime" in line:
+            val = line.split("=")[1]
+            walltime = sum(int(i) * 60 ** index for index, i in enumerate(val.split(":")[::-1]))
+        elif "resources_used.cput" in line:
+            val = line.split("=")[1]
+            cpu_time = sum(int(i) * 60 ** index for index, i in enumerate(val.split(":")[::-1]))
+            cflag = True
+        elif "resources_used.ncpus" in line:
+            ncpus = line.split("=")[1]
+        elif "Date: " in line:
+            job_time = line.split(": ")[1]
+        elif "PBS Job Id" in line:
+            job_id = line.split(": ")[1]
+        elif "Job Name" in line:
+            job_name = line.split(": ")[1]
+        elif "Execution" in line or "execution" in line:
+            exe_status = line
+            LOG.info(str(exe_status))
+        elif "Exit_status" in line:
+            exit_status = line.split("=")[1]
+        elif "resources_used.mem" in line:
+            mem_used = line.split("=")[1]
+        elif "resources_used.vmem" in line:
+            vmem_used = line.split("=")[1]
+
+    # Do not perform job efficiency calculation for the jobs currently executing or just started
+    if cflag:
+        job_efficiency = (float(cpu_time) / float(walltime) / float(ncpus)) * 100
+
+    return (cflag, job_efficiency, ncpus, job_time, job_id, job_name, exe_status,
+            exit_status, mem_used, vmem_used)
 
 
 def handler(event, context):
@@ -74,61 +113,27 @@ def handler(event, context):
     new_email = [(record['s3']['bucket']['name'], urllib.parse.unquote(record['s3']['object']['key'])) for record in
                  event.get('Records', [])]
 
-    logger.info("Changed/new object detected in the s3 bucket: %s", str(new_email))
+    LOG.info("Changed/new object detected in the s3 bucket: %s", str(new_email))
 
     if new_email:
-        cpu_time = walltime = ncpus = exit_status = 0, 0, 0, 0
-        job_id = job_name = exe_status = "NA", "NA", "NA"
-        mem_used = vmem_used = "0kb", "0kb"
         # TODO
         # Fetch number of dataset processed by the PBS job and
         # calculate datasets processing efficiency. And write the updated metadata
         # document into AWS ES.
         # ndatasets = datasets_eff = "NA", "NA"
 
-        # Enable CPU calculation flag, if we have walltime, cputime, and ncpus in the email body
-        cflag = False
-
         for bucket, s3_key in new_email:
-            logger.info(f"Processing s3://{bucket}/{s3_key}")
-            data = s3.Object(bucket, s3_key)
+            LOG.info("Processing s3://{%s}/{%s}", str(bucket), str(s3_key))
+            email_body = S3.Object(bucket, s3_key).get()['Body'].read().decode('utf-8')
 
-            email_body = data.get()['Body'].read().decode('utf-8')
-
-            # Report when the job started
-            for line in email_body.split('\r\n'):
-                if "resources_used.walltime" in line:
-                    val = line.split("=")[1]
-                    walltime = sum(int(i) * 60 ** index for index, i in enumerate(val.split(":")[::-1]))
-                elif "resources_used.cput" in line:
-                    val = line.split("=")[1]
-                    cpu_time = sum(int(i) * 60 ** index for index, i in enumerate(val.split(":")[::-1]))
-                    cflag = True
-                elif "resources_used.ncpus" in line:
-                    ncpus = line.split("=")[1]
-                elif "Date: " in line:
-                    job_time = line.split(": ")[1]
-                elif "PBS Job Id" in line:
-                    job_id = line.split(": ")[1]
-                elif "Job Name" in line:
-                    job_name = line.split(": ")[1]
-                elif "Execution" in line or "execution" in line:
-                    exe_status = line
-                    logger.info(f"{exe_status}")
-                elif "Exit_status" in line:
-                    exit_status = line.split("=")[1]
-                elif "resources_used.mem" in line:
-                    mem_used = line.split("=")[1]
-                elif "resources_used.vmem" in line:
-                    vmem_used = line.split("=")[1]
+            (cflag, job_efficiency, ncpus,
+             job_time, job_id, job_name, exe_status,
+             exit_status, mem_used, vmem_used) = _fetch_job_info(email_body)
 
             if cflag:
-                job_efficiency = (cpu_time / walltime / float(ncpus)) * 100
-                logger.info(f"CPU Efficiency = {job_efficiency}%")
-
                 # Push the metadata coming from every trigger generated by
                 # object creation events in S3 bucket
-                push_metadata_to_es({
+                _push_metadata_to_es({
                     'pbsjob_raijin': {
                         "info": {
                             'job_id': job_id.strip(),
@@ -149,4 +154,4 @@ def handler(event, context):
                     },
                 })
     else:
-        logger.info("No new/updated email record found in the S3 bucket")
+        LOG.info("No new/updated email record found in the S3 bucket")
