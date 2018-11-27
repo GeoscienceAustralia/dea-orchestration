c2fdf3481b295b3379ab25473df9e770b346b021
Santosh Mohan <santosh.mohan@ga.gov.au>
2018-11-27 12:40:13 +1100

    Fix travis failures

---
+++ lambda_modules/dea_raijin/dea_raijin/auth/__init__.py
@@ -0,0 +1,2 @@
+from .raijin_ssh import RaijinSession, SSHConfig
+from .ssm_key_retrieve import get_ssm_parameter
---
+++ lambda_modules/dea_raijin/dea_raijin/auth/ssm_key_retrieve.py
@@ -0,0 +1,28 @@
+import boto3
+
+from ..config import AWS_REGION
+
+SSM_SERVICE_NAME = 'ssm'
+
+SSM = boto3.client(SSM_SERVICE_NAME, AWS_REGION)
+
+
+def get_ssm_parameter(name, with_decryption=True):
+    """ Returns a parameter from the Secure Systems Manager
+
+    Args:
+        name (str): Key name from the ssm
+        with_decryption (bool): if the value should be decrypted with default kms key
+
+    Returns:
+        str: The value of the key in the ssm
+
+    Raises:
+        AttributeError: If key doesn't exist in the ssm.
+    """
+
+    response = SSM.get_parameters(Names=[name], WithDecryption=with_decryption)
+
+    if response:
+        return response['Parameters'][0]['Value']  # Return the first value from doc stub
+    raise AttributeError("Key '{}' not found in region '{}'".format(name, AWS_REGION))
