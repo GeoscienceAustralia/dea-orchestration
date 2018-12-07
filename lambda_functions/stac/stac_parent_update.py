591d18b068b93f2e43154d10f1b628a046ab7d7b
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-12-07 14:27:21 +1100

    configurable stac catalog structure done

--- lambda_functions/stac/stac_parent_update.py
+++ lambda_functions/stac/stac_parent_update.py
@@ -80,19 +80,13 @@ def incremental_list(inventory_s3_keys, from_date):
 class CatalogUpdater:
     """
     Collate all the new links to be added and then update S3
-    Assumed structure:
-            root catalog
-                -> per product/catalog.json
-                    -> x/catalog.json
-                        -> y/catalog.json
     """
 
     def __init__(self, config):
         self.config = config
-        self.y_catalogs = {}
-        self.x_catalogs = {}
-        self.top_level_catalogs = {}
-        self.catalogs = {}
+        self.catalogs_to_items = {}
+        self.mid_level_catalogs = {}
+        self.collection_catalogs = {}
 
     def update_parents_all(self, s3_keys, bucket):
         """
@@ -100,105 +94,117 @@ class CatalogUpdater:
         """
 
         for item in s3_keys:
+
+            prod_dict = self.search_product_in_config(item)
+
+            prefixes = self.get_prefixes(prod_dict['catalog_structure'], item)
+            collection_prefix = str(Path(prefixes[0]).parent)
+
             s3_key = Path(item)
-            # Collate parent catalog links
-            self.add_to_y_catalog_links(f'{s3_key.parent}/{s3_key.stem}_STAC.json')
+
+            # Add to top mid level catalog that got parent pointing to collection catalog
+            if len(prefixes) == 1:
+                child_catalog_name = f'{s3_key.parent}/{s3_key.stem}_STAC.json'
+            else:
+                child_catalog_name = f'{prefixes[1]}/catalog.json'
+            self.add_to_catalog(self.mid_level_catalogs, prefixes[0],
+                                f'{collection_prefix}/catalog.json', child_catalog_name)
+
+            # Add to last catalog level that hold links to items
+            if len(prefixes) == 1:
+                parent_catalog_name = f'{collection_prefix}/catalog.json'
+            else:
+                parent_catalog_name = f'{prefixes[-2]}/catalog.json'
+            self.add_to_catalog(self.catalogs_to_items, prefixes[-1],
+                                parent_catalog_name, f'{s3_key.parent}/{s3_key.stem}_STAC.json')
+
+            # Add to in between top level and last level catalogs
+            for count, catalog_prefix in enumerate(prefixes[1:-1]):
+                self.add_to_catalog(self.mid_level_catalogs, catalog_prefix,
+                                    f'{prefixes[count]}/catalog.json',
+                                    f'{prefixes[count + 2]}/catalog.json')
+
+            # Add to collection catalog
+            if self.collection_catalogs.get(collection_prefix):
+                self.collection_catalogs[collection_prefix].add(f'{prefixes[0]}/catalog.json')
+            else:
+                self.collection_catalogs[collection_prefix] = {f'{prefixes[0]}/catalog.json'}
 
         # Update catalog files in s3 bucket now
-        self.update_all_y_s3(bucket)
-        self.update_all_x_s3(bucket)
-        self.update_all_top_level_s3(bucket)
+        self.update_collection_catalogs(bucket)
+        self.update_mid_level_catalogs(bucket)
+        self.update_catalogs_to_items(bucket)
 
-    def add_to_y_catalog_links(self, s3_key):
+    @staticmethod
+    def get_prefixes(templates, item):
         """
-        Add the given s3 key to corresponding y catalog links
+        Get s3 prefixes corresponding to each catalog template
         """
 
-        template = '{prefix}/x_{x}/y_{y}/{}'
-        params = pparse(template, s3_key)
-        if params:
-            params = params.named
+        prefixes = []
+        for template in templates:
+            template_ = '{prefix}/' + template + '/{}'
+            params = pparse(template_, item)
+            if not params:
+                raise NameError('Catalog template parsing error: ' + item)
+            prefixes.append(('{prefix}/' + template).format(**params.named))
+        return prefixes
 
-            y_catalog_prefix = f'{params["prefix"]}/x_{params["x"]}/y_{params["y"]}'
+    @staticmethod
+    def add_to_catalog(catalog_dict, catalog_prefix, parent_catalog_name, item):
+        """
+        Add a catalog file name to a catalog dict
+        """
 
-            if self.y_catalogs.get(y_catalog_prefix):
-                self.y_catalogs[y_catalog_prefix].add(s3_key)
-            else:
-                self.y_catalogs[y_catalog_prefix] = {s3_key}
+        if catalog_dict.get(catalog_prefix):
+            if catalog_dict[catalog_prefix]['parent'] != parent_catalog_name:
+                raise NameError('Incorrect parent catalog name for : ' + item)
+            catalog_dict[catalog_prefix]['links'].add(item)
+        else:
+            catalog_dict[catalog_prefix] = {'parent': parent_catalog_name, 'links': {item}}
 
-            # Add the y catalog name to the corresponding x catalog
-            self.add_to_x_catalog_links(f'{y_catalog_prefix}/catalog.json')
-
-    def update_all_y_s3(self, bucket):
+    def update_mid_level_catalogs(self, bucket):
         """
         Update all the catalogs in S3 that has updated links
         """
 
         s3_res = boto3.resource('s3')
-        for y_catalog_prefix in self.y_catalogs:
-            obj = s3_res.Object(bucket, y_catalog_prefix)
-
-            # Create y catalog
-            template = '{prefix}/y_{y}'
-            params = pparse(template, y_catalog_prefix).named
-            y_catalog_parent_prefix = params['prefix']
-            y_catalog = self.create_catalog(y_catalog_prefix,
-                                            y_catalog_parent_prefix,
-                                            'List of items')
+        for catalog_prefix in self.mid_level_catalogs:
+            # Create the catalog
+            catalog = self.create_catalog(catalog_prefix,
+                                          self.mid_level_catalogs[catalog_prefix]['parent'],
+                                          'List of Directories')
 
             # Add the links
-            for link in self.y_catalogs[y_catalog_prefix]:
-                y_catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'item'})
+            for link in self.mid_level_catalogs[catalog_prefix]['links']:
+                catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'child'})
 
             # Put y_catalog dict to s3
-            obj = s3_res.Object(bucket, f'{y_catalog_prefix}/catalog.json')
-            obj.put(Body=json.dumps(y_catalog), ContentType='application/json')
+            obj = s3_res.Object(bucket, f'{catalog_prefix}/catalog.json')
+            obj.put(Body=json.dumps(catalog), ContentType='application/json')
 
-    def add_to_x_catalog_links(self, y_catalog_name_abs):
-        """
-        Add the given y catalog s3 key link to the corresponding x catalog links
-        """
-
-        template = '{prefix}/x_{x}/{}'
-        params = pparse(template, y_catalog_name_abs).named
-        x_catalog_prefix = f'{params["prefix"]}/x_{params["x"]}'
-
-        if self.x_catalogs.get(x_catalog_prefix):
-            self.x_catalogs[x_catalog_prefix].add(y_catalog_name_abs)
-        else:
-            self.x_catalogs[x_catalog_prefix] = {y_catalog_name_abs}
-
-        # Add x catalog link to product catalog
-        if self.top_level_catalogs.get(params['prefix']):
-            self.top_level_catalogs[params['prefix']].add(f'{x_catalog_prefix}/catalog.json')
-        else:
-            self.top_level_catalogs[params['prefix']] = {f'{x_catalog_prefix}/catalog.json'}
-
-    def update_all_x_s3(self, bucket):
+    def update_catalogs_to_items(self, bucket):
         """
         Update all the x catalogs in S3 that has updated links
         """
 
         s3_res = boto3.resource('s3')
-        for x_catalog_prefix in self.x_catalogs:
+        for catalog_prefix in self.catalogs_to_items:
 
-            # Create x catalog
-            template = '{prefix}/x_{x}'
-            params = pparse(template, x_catalog_prefix).named
-            x_catalog_parent_prefix = params['prefix']
-            x_catalog = self.create_catalog(x_catalog_prefix,
-                                            x_catalog_parent_prefix,
-                                            'List of Sub Directories')
+            # Create catalog
+            catalog = self.create_catalog(catalog_prefix,
+                                          self.catalogs_to_items[catalog_prefix]['parent'],
+                                          'List of items')
 
             # update the links
-            for link in self.x_catalogs[x_catalog_prefix]:
-                x_catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'child'})
+            for link in self.catalogs_to_items[catalog_prefix]['links']:
+                catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'item'})
 
-            # Put x_catalog dict to s3
-            obj = s3_res.Object(bucket, f'{x_catalog_prefix}/catalog.json')
-            obj.put(Body=json.dumps(x_catalog), ContentType='application/json')
+            # Put catalog dict to s3
+            obj = s3_res.Object(bucket, f'{catalog_prefix}/catalog.json')
+            obj.put(Body=json.dumps(catalog), ContentType='application/json')
 
-    def create_catalog(self, prefix, parent_prefix, description):
+    def create_catalog(self, prefix, parent_catalog_name, description):
         """
         Create a STAC catalog
         """
@@ -210,7 +216,7 @@ class CatalogUpdater:
             ('links', [
                 {'href': f'{self.config["aws-domain"]}/{prefix}/catalog.json',
                  'ref': 'self'},
-                {'href': f'{self.config["aws-domain"]}/{parent_prefix}/catalog.json',
+                {'href': f'{self.config["aws-domain"]}/{parent_catalog_name}',
                  'rel': 'parent'},
                 {'href': self.config["root-catalog"],
                  'rel': 'root'}
@@ -229,7 +235,7 @@ class CatalogUpdater:
                     return product_dict
         return None
 
-    def update_all_top_level_s3(self, bucket):
+    def update_collection_catalogs(self, bucket):
         """
         Update all the parent catalogs one level above x dir in s3. These are
         STAC collection catalogs. Please see
@@ -239,10 +245,10 @@ class CatalogUpdater:
 
         s3_res = boto3.resource('s3')
 
-        for top_level in self.top_level_catalogs:
-            product_type = Path(top_level).parts[0]
-            top_level_catalog_name = f'{top_level}/catalog.json'
-            info = self.search_product_in_config(top_level)
+        for collection_prefix in self.collection_catalogs:
+            product_type = Path(collection_prefix).parts[0]
+            collection_catalog_name = f'{collection_prefix}/catalog.json'
+            info = self.search_product_in_config(collection_prefix)
 
             # get extents from config
             extent = info.get('extent')
@@ -251,32 +257,32 @@ class CatalogUpdater:
             spatial_extent = extent.get('spatial', spatial_extent) if extent else spatial_extent
             temporal_extent = extent.get('temporal', temporal_extent) if extent else temporal_extent
 
-            # create the top level catalog
-            top_level_catalog = OrderedDict([
+            # create the collection catalog
+            collection_catalog = OrderedDict([
                 ('stac_version', '0.6.0'),
                 ('id', info['name']),
                 ('description', info['description'])])
             if info.get('keywords'):
-                top_level_catalog['keywords'] = info['keywords']
+                collection_catalog['keywords'] = info['keywords']
             if info.get('version'):
-                top_level_catalog['version'] = info['version']
-            top_level_catalog['license'] = self.config['license']['short_name']
+                collection_catalog['version'] = info['version']
+            collection_catalog['license'] = self.config['license']['short_name']
             if info.get('providers'):
-                top_level_catalog['providers'] = info.get('providers')
-            top_level_catalog['extent'] = {'spatial': spatial_extent, 'temporal': temporal_extent}
-            top_level_catalog['links'] = [
-                {'href': f'{self.config["aws-domain"]}/{top_level_catalog_name}', 'ref': 'self'},
+                collection_catalog['providers'] = info.get('providers')
+            collection_catalog['extent'] = {'spatial': spatial_extent, 'temporal': temporal_extent}
+            collection_catalog['links'] = [
+                {'href': f'{self.config["aws-domain"]}/{collection_catalog_name}', 'ref': 'self'},
                 {'href': f'{self.config["aws-domain"]}/{product_type}/catalog.json', 'rel': 'parent'},
                 {'href': self.config["root-catalog"], 'rel': 'root'}
             ]
 
             # Update the links
-            for link in self.top_level_catalogs[top_level]:
-                top_level_catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'child'})
+            for link in self.collection_catalogs[collection_prefix]:
+                collection_catalog['links'].append({'href': f'{self.config["aws-domain"]}/{link}', 'rel': 'child'})
 
-            # Put top level catalog to s3
-            obj = s3_res.Object(bucket, top_level_catalog_name)
-            obj.put(Body=json.dumps(top_level_catalog), ContentType='application/json')
+            # Put collection catalog to s3
+            obj = s3_res.Object(bucket, collection_catalog_name)
+            obj.put(Body=json.dumps(collection_catalog), ContentType='application/json')
 
 
 if __name__ == '__main__':
