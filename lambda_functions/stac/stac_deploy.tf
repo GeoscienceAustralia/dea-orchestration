fe7401e3d742e78272524bb6a61036edaf859419
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2019-01-08 15:57:55 +1100

    brought s3 bucket policy under terraform

--- lambda_functions/stac/stac_deploy.tf
+++ lambda_functions/stac/stac_deploy.tf
@@ -32,6 +32,49 @@ data "aws_s3_bucket" "dea_s3_bucket" {
   bucket = "dea-public-data-dev"
 }
 
+resource "aws_s3_bucket_policy" "dea_public_data_policy" {
+  bucket = "${data.aws_s3_bucket.dea_s3_bucket.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPublicRead",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${data.aws_s3_bucket.dea_s3_bucket.bucket}/*"
+        },
+        {
+            "Sid": "AllowPublicList",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:ListBucket",
+            "Resource": "arn:aws:s3:::${data.aws_s3_bucket.dea_s3_bucket.bucket}"
+        },
+        {
+            "Sid": "AllowDevAcctNotificationAdd",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::451924316694:root"
+            },
+            "Action": [
+                "s3:GetBucketLocation",
+                "s3:ListBucket",
+                "s3:GetBucketNotification",
+                "s3:PutBucketNotification"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${data.aws_s3_bucket.dea_s3_bucket.bucket}",
+                "arn:aws:s3:::${data.aws_s3_bucket.dea_s3_bucket.bucket}/*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
 resource "aws_s3_bucket_notification" "yaml_notification" {
   bucket = "${data.aws_s3_bucket.dea_s3_bucket.id}"
 
