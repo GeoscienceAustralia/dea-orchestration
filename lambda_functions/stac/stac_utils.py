45b232361240e9bbb2c386118c5a99674585bc60
Damien Ayers <damien@omad.net>
2019-04-01 15:54:08 +1100

    Don't use pathlib when str function suffices

--- lambda_functions/stac/stac_utils.py
+++ lambda_functions/stac/stac_utils.py
@@ -1,14 +1,15 @@
 import dateutil.parser
-from pathlib import PurePosixPath
 
 
 def yamls_in_inventory_list(keys, cfg):
     """
     Return generator of yaml files in s3 of products that belong to 'aws-products' in GLOBAL_CONFIG
     """
-    prefixes = set(p['prefix'] for p in cfg['products'])
+    prefixes = set(p['prefix']
+                   for p in cfg['products'])
     for item in keys:
-        if any(item.Key.startswith(prefix) for prefix in prefixes) and PurePosixPath(item.key).suffix == '.yaml':
+        if item.key.endswith('.yaml') and any(item.Key.startswith(prefix)
+                                              for prefix in prefixes):
             yield item.Key
 
 
