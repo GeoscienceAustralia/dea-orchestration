87bd3882305fae87e66058816989523b88679cc4
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-06-06 18:34:54 +1000

    Add pytests

---
+++ lambda_functions/automate_cubedash_db_update/test_handler.py
@@ -0,0 +1,16 @@
+from handler import _update_cubedash_db_config
+
+
+def test_execute_command():
+    execs = []
+
+    def mock_execute_update_cubedash_config_command(*args, **kwargs):
+        execs.append('foo execute command')
+
+        return "", "", 0
+
+    output = _update_cubedash_db_config('test/file', 'test_dbname', 'test_branch', mock_execute_update_cubedash_config_command)
+
+    assert len(execs) == 1
+    assert execs[0].startswith('foo execute')
+    assert not output
