c2abb0c25edf0fa7960852056693d8d0ae3cfdbb
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-10-01 17:35:48 +1000

    Add pytest to verify handler.exec_command arguments

--- lambda_functions/automate_cubedash_db_update/test_handler.py
+++ lambda_functions/automate_cubedash_db_update/test_handler.py
@@ -1,6 +1,20 @@
 import unittest
+from unittest.mock import patch as Patch, call
 from mock import patch
 from handler import _update_cubedash_db_config, handler
+from ssh import _connect
+
+
+@Patch('handler.exec_command')
+def test_handler_exec_command(exec_command_mock):
+    exec_command_mock.return_value = "", "", 0
+
+    # Run the Function
+    _update_cubedash_db_config('update_file', 'ows_20190101', 'prod', execute_command=exec_command_mock)
+
+    # Check that the correct arguments are passed to handler.execute_command
+    assert exec_command_mock.call_args == call(
+        'execute_update_cubedash_config --file update_file --dbname ows_20190101 --bitbucket-branch prod')
 
 
 def test_execute_command():
