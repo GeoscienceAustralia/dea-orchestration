86e7fe54badf8475c9ba0700df041f342172a76b
Damien Ayers <damien@omad.net>
2019-11-07 15:31:29 +1100

    Fix shellcheck errors

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,15 @@
 npm install -g serverless
 
 for d in lambda_functions/*; do
-    pushd ${d}
+    pushd "${d}" || exit 1
 
     npm install
     serverless config credentials --provider aws --key "${AWS_ACCESS_KEY_ID}" --secret "${AWS_SECRET_ACCESS_KEY}" --profile prodProfile
     serverless deploy -v --stage prod
 
-    popd
+    popd || exit 1
 done
 
-cd lambda_functions/execute_ssh_command_js/
+cd lambda_functions/execute_ssh_command_js/ || exit 1
 
 serverless invoke --log --stage prod --function git_pull_prod
