2458382726f609d7f451c93b325d04c8b752a6b5
Alex Leith <alexgleith@gmail.com>
2020-01-14 14:48:14 +1100

    rename stuff

---
+++ raijin_scripts/execute_s2_to_s3_rolling/execute_s2_to_s3_rolling.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Set up PBS job variables
+#PBS -P v10
+#PBS -q copyq
+
+#PBS -l wd
+
+## Resource limits
+#PBS -l mem=1GB
+#PBS -l ncpus=1
+#PBS -l walltime=10:00:00
+
+## The requested job scratch space.
+#PBS -l jobfs=1GB
+#PBS -lstorage=gdata/if87
+
+# echo on and exit on fail
+set -ex
+
+# Set up our environment
+source "$HOME"/.bashrc
+
+# Load the latest stable DEA module
+module use /g/data/v10/public/modules/modulefiles
+module load dea
+
+python3 s2_to_s3_rolling.py "${num_days}" "${s3_bucket}" "${end_date}"
