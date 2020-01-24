4f736085ba7f6de5aab4b710cb7083c99851c7fa
Damien Ayers <damien@omad.net>
2020-01-24 16:35:55 +1100

    Record the original dataset id

--- raijin_scripts/execute_s2_to_s3_rolling/s2_to_s3_rolling.py
+++ raijin_scripts/execute_s2_to_s3_rolling/s2_to_s3_rolling.py
@@ -95,6 +95,7 @@ def replace_metadata(granule, s3_bucket, s3_metadata_path):
     del temp_metadata['lineage']
     temp_metadata['creation_dt'] = temp_metadata['extent']['center_dt']
     temp_metadata['product_type'] = 'S2MSIARD_NBAR'
+    temp_metadata['original_id'] = temp_metadata['id']
 
     # Create dataset ID based on Kirill's magic
     temp_metadata['id'] = str(odc_uuid("s2_to_s3_rolling", "1.0.0", [temp_metadata['id']]))
